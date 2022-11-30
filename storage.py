a6be4b9c6e7c425c8de15b8785345fc981b163f4
qm1CSU <quinn_matthew1@columbusstate.edu>
2022-11-30 23:18:20 +0000

    Modified storage.py

--- storage.py
+++ storage.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import io 
 import os       
 import csv
 
@@ -8,14 +9,10 @@ IMG_FOLDER = os.path.join('static', 'pics')
 
 app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 
-
-
 @app.route("/")
 def Display_IMG():
      storage = os.path.join(app.config['UPLOAD_FOLDER'], 'storage.jpg')
      return render_template("homepage.html", user_image=storage)
-
-
 
 @app.route("/")                
 def home():                    
@@ -26,7 +23,6 @@ def Open_IMG():
      ryzen = os.path.join(app.config['UPLOAD_FOLDER'], 'ryzen.jpg')
      return render_template("CPU.html", user_image=ryzen)
 
-
 @app.route("/CPU")                
 def CPU():                    
     return render_template("CPU.html")
@@ -36,20 +32,19 @@ def Show_IMG():
      rtx4090 = os.path.join(app.config['UPLOAD_FOLDER'], 'rtx4090.png')
      return render_template("GPU.html", user_image=rtx4090)
 
-
 @app.route("/GPU")                
 def GPU():                    
     return render_template("GPU.html")
 
 @app.route("/GPU")
 def readGPU():
-    with open('gpu.csv') as csv_file:
-        data = csv.reader(csv_file, delimeter=',')
+    with open('data/gpu.csv') as csv_file:
+        data = csv.reader(csv_file, delimiter=',') 
         first_line = True
         gpuData = [] 
-        for row in gpuData:   
-            if not first_line: 
-                items.append({
+        for row in data:   
+            if not first_line:  
+                gpuData.append({
                 "product_id": row[0], 
                 "product_type":row[1], 
                 "product_sku": row[2], 
@@ -67,24 +62,18 @@ def view_IMG():
      ram = os.path.join(app.config['UPLOAD_FOLDER'], 'ram.jpg')
      return render_template("ram.html", user_image=ram)
 
-
 @app.route("/ram")                
 def ram():                    
     return render_template("ram.html")
-
-
 
 @app.route("/ps")
 def wide_IMG():
      ps = os.path.join(app.config['UPLOAD_FOLDER'], 'ps.jpg')
      return render_template("ps.html", user_image=ps)
 
-
 @app.route("/ps")                
 def ps():                    
     return render_template("ps.html")
                                         
-                                           
-    
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
