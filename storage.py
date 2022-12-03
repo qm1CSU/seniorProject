dd207eed31988d5c9395ad983e3147b3c42f545a
qm1CSU <quinn_matthew1@columbusstate.edu>
2022-12-03 08:07:44 +0000

    Final with read from db need write

--- storage.py
+++ storage.py
@@ -17,7 +17,33 @@ def Display_IMG():
 def home():                    
     return render_template("homepage.html")
 
-@app.route("/")
+@app.route("/CPU") 
+def readCPU():  
+    with open('data/cpu.csv') as file:  
+        data = csv.reader(file, delimiter=',') 
+        first_line = True
+        cpuData = []
+        for row in data: 
+            if not first_line: 
+                cpuData.append({
+                    "product_id": row[0],
+                    "product_type": row[1],
+                    "product_sku": row[2],
+                    "make": row[3],
+                    "model": row[4],
+                    "price": row[5],
+                    "quantity": row[6],
+                })     
+            else:
+                first_line = False
+        print(cpuData) 
+        return render_template("CPU.html", cpuData=cpuData)
+
+@app.route("/CPU")                
+def CPU():                     
+    return render_template("CPU.html")
+
+@app.route("/GPU")
 def readGPU():  
     with open('data/gpu.csv') as file:  
         data = csv.reader(file, delimiter=',') 
@@ -36,42 +62,63 @@ def readGPU():
                 })     
             else:
                 first_line = False
-        return render_template("GPU.html", gpuData=gpuData)
+        print(gpuData) 
+        return render_template("GPU.html", gpuData=gpuData)         
 
-@app.route("/CPU")
-def Open_IMG():
-     ryzen = os.path.join(app.config['UPLOAD_FOLDER'], 'ryzen.jpg')
-     return render_template("CPU.html", user_image=ryzen)
+@app.route("/GPU") 
+def GPU():  
+    return render_template("GPU.html") 
 
-@app.route("/CPU")                
-def CPU():                    
-    return render_template("CPU.html")
+@app.route("/ram")  
+def readRAM():  
+    with open('data/ram.csv') as file:  
+        data = csv.reader(file, delimiter=',') 
+        first_line = True
+        ramData = []
+        for row in data: 
+            if not first_line: 
+                ramData.append({
+                    "product_id": row[0],
+                    "product_type": row[1],
+                    "product_sku": row[2],
+                    "make": row[3],
+                    "model": row[4],
+                    "price": row[5],
+                    "quantity": row[6],
+                })     
+            else:
+                first_line = False
+        print(ramData) 
+        return render_template("ram.html", ramData=ramData) 
 
-@app.route("/GPU")
-def Show_IMG():
-     rtx4090 = os.path.join(app.config['UPLOAD_FOLDER'], 'rtx4090.png')
-     return render_template("GPU.html", user_image=rtx4090)
-
-@app.route("/GPU")                
-def GPU():                    
-    return render_template("GPU.html")         
-
-@app.route("/ram")
-def view_IMG():
-     ram = os.path.join(app.config['UPLOAD_FOLDER'], 'ram.jpg')
-     return render_template("ram.html", user_image=ram)
-
-@app.route("/ram")                
+@app.route("/ram")                 
 def ram():                    
     return render_template("ram.html")
 
-@app.route("/ps")
-def wide_IMG():
-     ps = os.path.join(app.config['UPLOAD_FOLDER'], 'ps.jpg')
-     return render_template("ps.html", user_image=ps)
+@app.route("/ps") 
+def readPS():  
+    with open('data/ps.csv') as file:  
+        data = csv.reader(file, delimiter=',') 
+        first_line = True
+        psData = []
+        for row in data: 
+            if not first_line: 
+                psData.append({
+                    "product_id": row[0],
+                    "product_type": row[1],
+                    "product_sku": row[2],
+                    "make": row[3],
+                    "model": row[4],
+                    "price": row[5],
+                    "quantity": row[6],
+                })     
+            else:
+                first_line = False
+        print(psData) 
+        return render_template("ps.html", psData=psData) 
 
 @app.route("/ps")                
-def ps():                    
+def ps():                     
     return render_template("ps.html")
                                         
 if __name__== "__main__":
